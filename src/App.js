43c6bf703f7d4d2354bcc348ae14bec9158e7cff
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-13 16:22:03 +0800

    configured webpack and added loaders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import 'App.css';
+import img from './img.jpg';
 
 const App = () => (
 	<div>
-		<h1>Hello React</h1>
+		<h1 className='blue'>Hello React</h1>
+		<img src={img} alt='' />
 	</div>
 );
 
